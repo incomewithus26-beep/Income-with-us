0a0ceefeb197cff9acc7a3f36f397337417dce6f
incomewithus26-beep <incomewithus26@gmail.com>
2026-09-20 14:05:57 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "YOUR_BOT_TOKEN"
+TOKEN = "8899030467:AAHVzUBdI_-HTiy55uwjLaV1OXkBUXcnOK8"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
